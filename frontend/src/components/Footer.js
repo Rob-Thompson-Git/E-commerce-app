35baf8f8b94779fdccdf5aa93a17fd2d7a938283
RobThompson <rob.thomp15@gmail.com>
2022-12-21 07:04:46 -0600

    finished fixed footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,6 +1,14 @@
+import { Container, Row, Col } from "react-bootstrap";
+
 const Footer = () => {
     return (
-        "this is footer"
+    <footer>
+        <Container fluid>
+            <Row className="mt-5">
+                <Col className="bg-dark text-white text-center py-5"> Qwiks | Copyright &copy; 2022 | Qwik, and Easy!</Col>
+            </Row>
+        </Container>
+    </footer>
     )
 }
 
